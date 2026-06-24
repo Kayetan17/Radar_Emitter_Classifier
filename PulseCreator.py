38cf8a867ee54a63d7a9c24d45cded6f97186d3c
Kayetan Protas <kayetanp@gmail.com>
2026-06-24 14:50:27 -0400

    pulse creator done

--- PulseCreator.py
+++ PulseCreator.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 
-def generate_pulse_offsets():
-    print(np.random.normal(1000, 15, 8))  # PRI
-    print(np.random.normal(1.0, 0.08, 8))  # pulse widths
-    print(np.random.normal(9.0, 0.03, 8))  # frequency
+def generate_pulse_offsets(pri_center, pw_center, freq_center, n_pulses):
+    PRI = np.random.normal(pri_center, 15, n_pulses)  
+    pulseW = np.random.normal(pw_center, 0.08, n_pulses)  
+    frequency = np.random.normal(freq_center, 0.03, n_pulses)
+    
+    offset_pulse = np.stack([PRI, pulseW, frequency], axis=1)
+    
+    return offset_pulse
     
 
-generate_pulse_offsets()
+print(generate_pulse_offsets(1000, 1, 9))
+print(generate_pulse_offsets(2000, 2, 10))
+
     
 
 #def pulse1(PRI, pulseW, frequency): 
